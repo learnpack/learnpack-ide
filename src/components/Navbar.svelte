08d1d30f99d5e0a00d0ce8d2e774bc00c09fc7a1
Tomas <gonzalez.tommy2001@gmail.com>
2022-08-15 15:09:57 -0400

    Stylings added to the navbar

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,159 +1,167 @@
 <script>
-    export let exercises;
-    import Languages from "./Languages.svelte";
+  export let exercises;
+  import Languages from "./Languages.svelte";
 
-    function showExercises() {
-        let navbar = document.getElementById("navbar");
-        if (navbar.style.display === "block") {
-            navbar.style.display = "none";
-        } else {
-            navbar.style.display = "block";
-        }
+  function showExercises() {
+    let navbar = document.getElementById("navbar");
+    let nav = document.getElementById("nav")
+    if (navbar.style.display === "block") {
+      navbar.style.display = "none";
+      nav.style.height = "80px";
+    } else {
+      navbar.style.display = "block";
+      nav.style.height = "auto";
     }
+  }
 </script>
 
-<!-- <div class="navbar-component">
-    <div id="navbar">
-        {#each exercises as exercise}
-            <svelte:component this={exercise.component} {...exercise} />
-        {/each}
-    </div>
-    <button class="navbar-button" on:click={showExercises}><img src="https://icongr.am/fontawesome/align-justify.svg?size=25&color=ffffff"></button>
-    <br>
-    
-</div> -->
-
 <div>
-
-<nav>
-         <div class="menu">
-             <!-- svelte-ignore a11y-invalid-attribute -->
-            <a href="#" on:click={showExercises}><img alt="menu" src="https://icongr.am/fontawesome/align-justify.svg?size=25&color=ffffff"></a>
-         </div>
-         <div>
-         <ul>
-            <!-- <li><a class="active" id="flag" href="#"><Languages/></a></li> -->
-            <!-- svelte-ignore a11y-invalid-attribute -->
-            <li><a href="#"><img alt="play" src="https://icongr.am/fontawesome/play-circle-o.svg?size=30&color=ffffff"></a></li>
-            <!-- svelte-ignore a11y-invalid-attribute -->
-            <li><a href="#"><img alt="tutorial" src="https://icongr.am/fontawesome/graduation-cap.svg?size=30&color=ffffff"></a></li>
-            <!-- svelte-ignore a11y-invalid-attribute -->
-            <li><a href="#"><img alt="left-arrow" src="https://icongr.am/fontawesome/arrow-left.svg?size=30&color=ffffff"></a></li>
-            <!-- svelte-ignore a11y-invalid-attribute -->
-            <li><a href="#"><img alt="right arrow" src="https://icongr.am/fontawesome/arrow-right.svg?size=30&color=ffffff"></a></li>
-         </ul>
-         <div id="navbar">
-          {#each exercises as exercise}
-              <svelte:component this={exercise.component} {...exercise} />
-          {/each}
+  <nav id="nav">
+    <div class="row">
+      <div class="menu">
+        <!-- svelte-ignore a11y-invalid-attribute -->
+        <img
+            on:click={showExercises}
+            alt="menu"
+            src="https://icongr.am/fontawesome/align-justify.svg?size=25&color=ffffff"
+          />
       </div>
-        </div>
-      </nav>
-
-      
-
-
+      <div class="icon">
+        <img
+          alt="play"
+          src="https://icongr.am/fontawesome/play-circle-o.svg?size=30&color=ffffff"
+        />
+      </div>
+      <div class="icon">
+        <img
+          alt="tutorial"
+          src="https://icongr.am/fontawesome/graduation-cap.svg?size=30&color=ffffff"
+        />
+      </div>
+      <div class="icon">
+        <img
+          alt="left-arrow"
+          src="https://icongr.am/fontawesome/arrow-left.svg?size=30&color=ffffff"
+        />
+      </div>
+      <div class="icon">
+        <img
+          alt="right arrow"
+          src="https://icongr.am/fontawesome/arrow-right.svg?size=30&color=ffffff"
+        />
+      </div>
     </div>
+    <div id="navbar">
+      {#each exercises as exercise}
+        <svelte:component this={exercise.component} {...exercise} />
+      {/each}
+    </div>
+  </nav>
+</div>
 
 <style>
-
-
-
-*{
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-  font-family: 'Poppins', sans-serif;
-} 
-
-#flag{
-    font-size: 2rem;
-}
-
-nav{
-  display: flex;
-  height: 80px;
-  width: 100%;
-  background: #1b1b1b;
-  align-items: center;
-  justify-content: space-between;
-  padding: 0 50px 0 100px;
-  flex-wrap: wrap;
-}
-nav .menu{
-  color: #fff;
-  font-size: 35px;
-  font-weight: 600;
-}
-nav ul{
-  display: flex;
-  flex-wrap: wrap;
-  list-style: none;
-}
-nav ul li{
-  margin: 0 5px;
-}
-nav ul li a{
-  color: #f2f2f2;
-  text-decoration: none;
-  font-size: 18px;
-  font-weight: 500;
-  padding: 8px 15px;
-  border-radius: 5px;
-  letter-spacing: 1px;
-  transition: all 0.3s ease;
-}
-nav ul li a.active,
-nav ul li a:hover{
-  color: black;
-  
-}
-
-@media (max-width: 1000px){
-  nav{
-    padding: 0 40px 0 50px;
-  }
-}
-@media (max-width: 920px) {
-  nav .menu-btn i{
-    display: block;
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: "Poppins", sans-serif;
   }
 
-  nav ul{
-    position: fixed;
-    top: 80px;
-    left: -100%;
-    background: #111;
-    height: 100vh;
+  .row {
+    display: flex;
     width: 100%;
-    text-align: center;
-    display: block;
+  }
+
+  .row .icon {
+    width: 12%;
+    padding: 2.5%;
+  }
+
+  nav {
+    display: flex;
+    height: 80px;
+    width: 100%;
+    background: #1b1b1b;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0 50px 0 100px;
+    flex-wrap: wrap;
+  }
+  
+  nav .menu {
+    color: #fff;
+    font-size: 35px;
+    font-weight: 600;
+    width: 50%;
+    padding-left: 5%;
+    padding: 2.5%;
+  }
+  nav ul {
+    display: flex;
+    flex-wrap: wrap;
+    list-style: none;
+  }
+  nav ul li {
+    margin: 0 5px;
+  }
+  nav ul li a {
+    color: #f2f2f2;
+    text-decoration: none;
+    font-size: 18px;
+    font-weight: 500;
+    padding: 8px 15px;
+    border-radius: 5px;
+    letter-spacing: 1px;
     transition: all 0.3s ease;
   }
-  nav ul li{
-    width: 100%;
-    margin: 40px 0;
-  }
-  nav ul li a{
-    width: 100%;
-    margin-left: -100%;
-    display: block;
-    font-size: 20px;
-    transition: 0.6s cubic-bezier(0.68, -0.55, 0.265, 1.55);
-  }
-  #click:checked ~ ul li a{
-    margin-left: 0px;
-  }
   nav ul li a.active,
-  nav ul li a:hover{
-    background: none;
-    color: cyan;
+  nav ul li a:hover {
+    color: black;
   }
-}
 
-    #navbar {
-        display: none;
+  @media (max-width: 1000px) {
+    nav {
+      padding: 0 40px 0 50px;
+    }
+  }
+  @media (max-width: 920px) {
+    nav .menu-btn i {
+      display: block;
     }
 
+    nav ul {
+      position: fixed;
+      top: 80px;
+      left: -100%;
+      background: #111;
+      height: 100vh;
+      width: 100%;
+      text-align: center;
+      display: block;
+      transition: all 0.3s ease;
+    }
+    nav ul li {
+      width: 100%;
+      margin: 40px 0;
+    }
+    nav ul li a {
+      width: 100%;
+      margin-left: -100%;
+      display: block;
+      font-size: 20px;
+      transition: 0.6s cubic-bezier(0.68, -0.55, 0.265, 1.55);
+    }
+    #click:checked ~ ul li a {
+      margin-left: 0px;
+    }
+    nav ul li a.active,
+    nav ul li a:hover {
+      background: none;
+      color: cyan;
+    }
+  }
 
+  #navbar {
+    display: none;
+  }
 </style>
